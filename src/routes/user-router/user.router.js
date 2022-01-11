5c2b9610db98debc3b350d4dcfde009b5e7928fb
Saif Hossain <atonlymesaif@gmail.com>
2022-01-12 01:22:55 +0600

    user creating endpoint added

--- src/routes/user-router/user.router.js
+++ src/routes/user-router/user.router.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const User = require('../../models/user.model');
 
 const userRouter = express.Router();
 
 userRouter.post('/users/createUser', (req, res) => {
-    
-})
+    const { email, password } = req.body;
+
+    try {
+        const user = new User({
+            email,
+            password,
+        });
+
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(409).send();
+    }
+});
 
 module.exports = userRouter;
