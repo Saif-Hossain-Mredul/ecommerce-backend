853cb02dd6e4c6321d89416c1116209dbca73f82
Saif Hossain <atonlymesaif@gmail.com>
2022-01-14 00:15:13 +0600

    product router integrated to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose-connection.db');
 
 const userRouter = require('./routes/user-router/user.router');
+const productRouter = require('./routes/product-router/product.router');
 
 const app = express();
 const port = process.env.PORT || 5000;
