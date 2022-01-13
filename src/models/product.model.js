71e9c4c8d165e053e63c45af175cde93b733dbd7
Saif Hossain <atonlymesaif@gmail.com>
2022-01-14 02:01:01 +0600

    product update endpoint added

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema(
         brand: {
             type: String,
         },
-        description: {
+        specification: {
             type: String,
         },
         price: {
--- src/routes/product-router/product.router.js
+++ src/routes/product-router/product.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Product = require('../../models/product.model');
+const userRouter = require('../user-router/user.router');
 
 const productRouter = express.Router();
 
@@ -12,6 +13,38 @@ productRouter.post('/add-product', async (req, res) => {
         res.status(201).send(product);
     } catch (e) {
         res.status(409).send();
+    }
+});
+
+userRouter.patch('/product/:id', async (req, res) => {
+    const requestedUpdates = Object.keys(req.body);
+    const allowedUpdates = [
+        'name',
+        'category',
+        'brand',
+        'specification',
+        'price',
+    ];
+    const isAllowedToUpdate = requestedUpdates.every((update) =>
+        allowedUpdates.includes(update)
+    );
+
+    if (!isAllowedToUpdate) throw new Error('Invalid Update field.');
+
+    try {
+        const product = await Product.findById({ _id: req.params.id });
+
+        if (!product) throw new Error('Can not find product.');
+
+        requestedUpdates.forEach((updateField) => {
+            product[updateField] = req.body[updateField];
+        });
+
+        await product.save();
+
+        res.send(product);
+    } catch (e) {
+        res.status(400).send(e.message);
     }
 });
 
