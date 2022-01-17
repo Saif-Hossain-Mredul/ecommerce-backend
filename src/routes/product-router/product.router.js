d68f45a9feff87bd6505a36fb255f1fdde981a2e
Saif Hossain <atonlymesaif@gmail.com>
2022-01-17 20:23:25 +0600

    image upload completed

--- src/routes/product-router/product.router.js
+++ src/routes/product-router/product.router.js
@@ -5,12 +5,10 @@ const multer = require('multer');
 
 const auth = require('../../middlewares/auth.middleware');
 const Product = require('../../models/product.model');
-const previewImageUploader = require('./helper-functions/previewImageUploader.hf');
-const displayImageUploader = require('./helper-functions/displayImageUploader.hf');
-const otherImageUploader = require('./helper-functions/otherImageUploader.hf');
 
 const addProduct = require('./route-functions/add-product.rf');
 const updateProduct = require('./route-functions/update-product.rf');
+const imageUpload = require('./route-functions/image-upload.rf');
 
 const productRouter = express.Router();
 
@@ -45,44 +43,7 @@ const fileUpload = multer({
 productRouter.post(
     '/image-upload/:id/:imageField',
     fileUpload.array('images', 5),
-    async (req, res) => {
-        try {
-            const { id, imageField } = req.params;
-
-            const product = await Product.findOne({ _id: id });
-
-            if (!product) throw new Error('Product not found.');
-
-            if (imageField === 'previewImage') {
-                const result = await previewImageUploader(
-                    req.files[0],
-                    product,
-                    imageField
-                );
-                res.send(result);
-            } else if (imageField === 'displayImages') {
-                const result = await displayImageUploader(
-                    req.files,
-                    product,
-                    imageField
-                );
-
-                res.send(result);
-            } else if (imageField === 'otherImages') {
-                const result = await otherImageUploader(
-                    req.files,
-                    product,
-                    imageField
-                );
-
-                res.send(result);
-            } else {
-                throw new Error('Invalid field.');
-            }
-        } catch (e) {
-            res.status(409).send(e);
-        }
-    }
+    imageUpload
 );
 
 module.exports = productRouter;
---
+++ src/routes/product-router/route-functions/image-upload.rf.js
@@ -0,0 +1,45 @@
+const Product = require('../../../models/product.model');
+const previewImageUploader = require('../helper-functions/previewImageUploader.hf');
+const displayImageUploader = require('../helper-functions/displayImageUploader.hf');
+const otherImageUploader = require('../helper-functions/otherImageUploader.hf');
+
+const imageUpload = async (req, res) => {
+    try {
+        const { id, imageField } = req.params;
+
+        const product = await Product.findOne({ _id: id });
+
+        if (!product) throw new Error('Product not found.');
+
+        if (imageField === 'previewImage') {
+            const result = await previewImageUploader(
+                req.files[0],
+                product,
+                imageField
+            );
+            res.send(result);
+        } else if (imageField === 'displayImages') {
+            const result = await displayImageUploader(
+                req.files,
+                product,
+                imageField
+            );
+
+            res.send(result);
+        } else if (imageField === 'otherImages') {
+            const result = await otherImageUploader(
+                req.files,
+                product,
+                imageField
+            );
+
+            res.send(result);
+        } else {
+            throw new Error('Invalid field.');
+        }
+    } catch (e) {
+        res.status(409).send(e);
+    }
+};
+
+module.exports = imageUpload;
