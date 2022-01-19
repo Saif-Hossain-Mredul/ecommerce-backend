a5e3b59c393d8534886d30561810fa8fedb99310
Saif Hossain <atonlymesaif@gmail.com>
2022-01-19 21:37:42 +0600

    delete cart product added

--- src/routes/user-router/user.router.js
+++ src/routes/user-router/user.router.js
@@ -56,6 +56,25 @@ userRouter.patch('/profile/cart', auth, async (req, res) => {
     res.send();
 });
 
+// remove product from cart
+userRouter.delete('/profile/cart', auth, async (req, res) => {
+    const productIndex = req.user.inCart.findIndex((product) => {
+        return product.productId.toString() === req.body.productId;
+    });
+
+    if (req.user.inCart[productIndex].quantity === 1) {
+        req.user.inCart = req.user.inCart.filter(
+            (product) => product.productId.toString() !== req.body.productId
+        );
+    } else {
+        req.user.inCart[productIndex].quantity -= 1;
+    }
+
+    await req.user.save();
+
+    res.send();
+});
+
 // add image to profile
 userRouter.post(
     '/profile/add-profile-picture',
