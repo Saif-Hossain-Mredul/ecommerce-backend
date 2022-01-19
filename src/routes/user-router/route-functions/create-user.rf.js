d375ce5150841b0bd34d0637639025d7cf8d4ae7
Saif Hossain <atonlymesaif@gmail.com>
2022-01-19 13:25:45 +0600

    create user moved to route functions

---
+++ src/routes/user-router/route-functions/create-user.rf.js
@@ -0,0 +1,25 @@
+const User = require('../../../models/user.model');
+
+const createUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            throw new Error('Email and password is required to create an user');
+        }
+
+        const user = new User({
+            email,
+            password,
+        });
+
+        await user.save();
+        const token = await user.generateAuthToken();
+
+        res.status(201).send({ user, token });
+    } catch (e) {
+        res.status(409).send(e.message);
+    }
+};
+
+module.exports = createUser;
--- src/routes/user-router/user.router.js
+++ src/routes/user-router/user.router.js
@@ -2,31 +2,12 @@ const express = require('express');
 const User = require('../../models/user.model');
 
 const auth = require('../../middlewares/auth.middleware');
+const createUser = require('./route-functions/create-user.rf');
 
 const userRouter = express.Router();
 
 // creates new user
-userRouter.post('/sign-up', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        if (!email || !password) {
-            throw new Error('Email and password is required to create an user');
-        }
-
-        const user = new User({
-            email,
-            password,
-        });
-
-        await user.save();
-        const token = await user.generateAuthToken();
-
-        res.status(201).send({ user, token });
-    } catch (e) {
-        res.status(409).send(e.message);
-    }
-});
+userRouter.post('/sign-up', createUser);
 
 // sign in route
 userRouter.post('/sign-in', async (req, res) => {
