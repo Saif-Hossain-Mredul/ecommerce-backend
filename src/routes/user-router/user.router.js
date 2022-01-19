55f909edb0285b8a7cdb1217caddf70e7be68c40
Saif Hossain <atonlymesaif@gmail.com>
2022-01-20 01:17:38 +0600

    some modification

--- src/routes/user-router/user.router.js
+++ src/routes/user-router/user.router.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const User = require('../../models/user.model');
-const multer = require('multer');
-const cloudinary = require('cloudinary');
-const fileUpload = require('../../config/multer-config');
 
 const auth = require('../../middlewares/auth.middleware');
 const createUser = require('./route-functions/create-user.rf');
 const signInUser = require('./route-functions/sign-in-user.rf');
 const signOutUser = require('./route-functions/sign-out-user.rf');
 const getProfile = require('./route-functions/get-profile.rf');
-const uploadCloudinary = require('../product-router/helper-functions/uploadCloudinary.hf');
 const addProfilePicture = require('./route-functions/add-profile-picture.rf');
 const updateUser = require('./route-functions/update-user.rf');
 const addToCart = require('./route-functions/add-to-cart.rf');
