a99baf53fb82a901b6cb494481e91677643f399a
Saif Hossain <atonlymesaif@gmail.com>
2022-01-26 13:13:52 +0600

    env variable used in cloudinary config file

--- src/config/cloudinary-config.js
+++ src/config/cloudinary-config.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const SecretKeys = require('../secret-keys');
 
 cloudinary.config({
-    cloud_name: SecretKeys.CLOUDINARY_CLOUD_NAME,
-    api_key: SecretKeys.CLOUDINARY_API_KEY,
-    api_secret: SecretKeys.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
