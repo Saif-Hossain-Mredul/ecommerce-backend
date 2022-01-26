900dd97a5363475d4def5c385d7e6d913e58ca06
Saif Hossain <atonlymesaif@gmail.com>
2022-01-26 12:35:51 +0600

    package json updated with env-cmd

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/user-router/user.router');
 const productRouter = require('./routes/product-router/product.router');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
