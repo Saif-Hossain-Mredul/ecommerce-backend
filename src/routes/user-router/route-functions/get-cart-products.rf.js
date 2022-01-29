2b7ce71ef1568685f96903cf4b4f41bc212cdc8f
Saif Hossain <atonlymesaif@gmail.com>
2022-01-29 12:03:13 +0600

    error sending added to get-cart.rf.js

--- src/routes/user-router/route-functions/get-cart-products.rf.js
+++ src/routes/user-router/route-functions/get-cart-products.rf.js
@@ -2,7 +2,7 @@ const getCartProduct = (req, res) => {
     try {
         res.send(req.user.inCart);
     } catch (e) {
-        res.status(400).send();
+        res.status(400).send({ error: { status: 400, message: e.message } });
     }
 };
 
