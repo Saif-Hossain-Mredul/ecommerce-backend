700958516ae7b0b6143622c351bba17e17c74164
Saif Hossain <atonlymesaif@gmail.com>
2022-01-29 11:59:17 +0600

    error sending added to get-profile.rf.js

--- src/routes/user-router/route-functions/get-profile.rf.js
+++ src/routes/user-router/route-functions/get-profile.rf.js
@@ -2,7 +2,7 @@ const getProfile = async (req, res) => {
     try {
         res.send(req.user);
     } catch (e) {
-        res.status(500).send(e.message);
+        res.status(500).send({ error: { status: 500, message: e.message } });
     }
 }
 
