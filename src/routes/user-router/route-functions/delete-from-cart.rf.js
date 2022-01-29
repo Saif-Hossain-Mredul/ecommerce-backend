3802c92dd636f2e29480ce6aebbbc837027fa8f0
Saif Hossain <atonlymesaif@gmail.com>
2022-01-29 12:24:34 +0600

    error handling added in delete from cart and error message sending option also added

--- src/routes/user-router/route-functions/delete-from-cart.rf.js
+++ src/routes/user-router/route-functions/delete-from-cart.rf.js
@@ -1,19 +1,25 @@
 const deleteFromCart = async (req, res) => {
-    const productIndex = req.user.inCart.findIndex((product) => {
-        return product.productId.toString() === req.body.productId;
-    });
+    try {
+        const productIndex = req.user.inCart.findIndex((product) => {
+            return product.productId.toString() === req.body.productId;
+        });
 
-    if (req.user.inCart[productIndex].quantity === 1) {
-        req.user.inCart = req.user.inCart.filter(
-            (product) => product.productId.toString() !== req.body.productId
-        );
-    } else {
-        req.user.inCart[productIndex].quantity -= 1;
+        if (productIndex === -1) throw new Error('Product not found');
+
+        if (req.user.inCart[productIndex].quantity === 1) {
+            req.user.inCart = req.user.inCart.filter(
+                (product) => product.productId.toString() !== req.body.productId
+            );
+        } else {
+            req.user.inCart[productIndex].quantity -= 1;
+        }
+
+        await req.user.save();
+
+        res.status(204).send();
+    } catch (e) {
+        res.status(404).send({ error: { status: 404, message: e.message } });
     }
-
-    await req.user.save();
-
-    res.send();
 };
 
 module.exports = deleteFromCart;
