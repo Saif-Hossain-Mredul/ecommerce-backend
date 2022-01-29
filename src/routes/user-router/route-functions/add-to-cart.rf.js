514fa42d1b3146d65e52c42bd16c204b633bb838
Saif Hossain <atonlymesaif@gmail.com>
2022-01-29 12:15:54 +0600

    error sending added to add-to-cart.rf.js

--- src/routes/user-router/route-functions/add-to-cart.rf.js
+++ src/routes/user-router/route-functions/add-to-cart.rf.js
@@ -1,17 +1,21 @@
 const addToCart = async (req, res) => {
-    const productIndex = req.user.inCart.findIndex((product) => {
-        return product.productId.toString() === req.body.productId;
-    });
+    try {
+        const productIndex = req.user.inCart.findIndex((product) => {
+            return product.productId.toString() === req.body.productId;
+        });
 
-    if (productIndex === -1) {
-        req.user.inCart.push({ ...req.body });
-    } else {
-        req.user.inCart[productIndex].quantity += 1;
+        if (productIndex === -1) {
+            req.user.inCart.push({ ...req.body });
+        } else {
+            req.user.inCart[productIndex].quantity += 1;
+        }
+
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(400).send({ error: { status: 400, message: e.message } });
     }
-
-    await req.user.save();
-
-    res.send();
 };
 
 module.exports = addToCart;
