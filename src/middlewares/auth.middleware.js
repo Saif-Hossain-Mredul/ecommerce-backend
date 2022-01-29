0c5c0e8b17eb4a61d847eb770950d789414f6179
Saif Hossain <atonlymesaif@gmail.com>
2022-01-29 19:50:20 +0600

    ...

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -19,7 +19,7 @@ const auth = async (req, res, next) => {
         next();
     } catch (e) {
         res.status(401).send({
-            error: { status: 401, message: 'Unauthorized' },
+            error: { status: 401, message: e.message }
         });
     }
 };
