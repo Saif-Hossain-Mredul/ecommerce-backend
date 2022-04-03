fa4e8198e434314528901ad53b2261ec504dbf1c
Saif Hossain <atonlymesaif@gmail.com>
2022-04-03 23:07:57 +0600

    confirm order endpint created

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -78,7 +78,7 @@ const userSchema = new mongoose.Schema(
                 },
                 productName: {
                     type: String,
-                    required: true,
+                    // required: true,
                 },
                 price: {
                     type: Number,
