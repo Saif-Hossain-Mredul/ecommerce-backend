d242dc748ec142e978d00b18a505c03c882d95a4
Saif Hossain <atonlymesaif@gmail.com>
2022-04-03 23:09:49 +0600

    confirm order

---
+++ src/routes/user-router/route-functions/confirm-order.rf.js
@@ -0,0 +1,25 @@
+const confirmOrder = async (req, res) => {
+    try {
+        req.user.inCart.forEach((product) => {
+            req.user.purchasedProducts.push({
+                ...product,
+                reviewed: false,
+                productName: product.productName,
+                price: product.price,
+                quantity: product.quantity,
+            });
+        });
+
+        req.user.inCart = [];
+
+        await req.user.save();
+
+        res.send({
+            purchasedProducts: req.user.purchasedProducts.length,
+            result: 'Your order is created successfully',
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(400).send({ error: { status: 400, message: e.message } });
+    }
+};
--- src/routes/user-router/user.router.js
+++ src/routes/user-router/user.router.js
@@ -54,31 +54,7 @@ userRouter.delete('/profile/wishlist', auth, deleteFromWishlist);
 // Must include skip count in the url  ---> ?skip=10
 userRouter.get('/profile/wishlist', auth, getWishlist);
 
-userRouter.post('/profile/confirm-order', auth, async (req, res) => {
-    try {
-        req.user.inCart.forEach((product) => {
-            req.user.purchasedProducts.push({
-                ...product,
-                reviewed: false,
-                productName: product.productName,
-                price: product.price,
-                quantity: product.quantity,
-            });
-        });
-
-        req.user.inCart = [];
-
-        await req.user.save();
-
-        res.send({
-            purchasedProducts: req.user.purchasedProducts.length,
-            result: 'Your order is created successfully',
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(400).send({ error: { status: 400, message: e.message } });
-    }
-});
+userRouter.post('/profile/confirm-order', auth, confirmOrder);
 
 // add image to profile
 userRouter.post(
